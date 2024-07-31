052eb70f59009b9c42f6a7223eefa5c53c942fae
Tracy Cano <tracycano3.14@gmail.com>
2024-07-30 23:12:39 -0700

    step 3 for lesson 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/create'
+  get 'posts/new'
+  get 'posts/edit'
+  get 'posts/show'
+  get 'posts/update'
+  get 'posts/destroy'
   # resources :users
 
   root 'forums#index'
@@ -13,7 +19,11 @@ Rails.application.routes.draw do
   post '/users/:id/logon', to: 'users#logon', as: 'user_logon'
   delete '/users/logoff', to: 'users#logoff', as: 'user_logoff'
 
-  resources :forums
+  resources :forums do
+    resources :posts, shallow: true, except: [:index]
+    resources :subscriptions, shallow: true, except: [:index]
+  end
+  get '/subscriptions', to: 'subscriptions#index', as: 'subscriptions'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
