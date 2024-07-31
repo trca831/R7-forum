f62e5f50dd112c75b4e9b14a2a4cfbd36a49fb3e
Tracy Cano <tracycano3.14@gmail.com>
2024-07-30 22:44:14 -0700

    adding step 1 of lesson7

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # resources :users
+
+  root 'forums#index'
+
   get '/users', to: 'users#index', as: 'users'
   get '/users/new', to: 'users#new', as: 'new_user'
   get '/users/:id', to: 'users#show', as: 'user'
